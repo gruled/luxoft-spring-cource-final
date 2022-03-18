866cb3a374a8a47e8116f695de568bf3d25edae7
Dmitrii Chukarin <d.chukarin@haulmont.com>
2022-03-18 15:47:50 +0400

    Init project

---
+++ src/test/java/com/edch/luxoftspringcourcefinal/LuxoftSpringCourceFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edch.luxoftspringcourcefinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxoftSpringCourceFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
